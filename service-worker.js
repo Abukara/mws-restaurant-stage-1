cbf0efdcb1681b609bf884454454af05f89ef3e9
Abukara <Lukas.Forst48@googlemail.com>
2018-04-06 22:00:18 +0200

    Add files via upload
    changed a few margins.

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,35 @@
+
+
+var staticCacheName = 'pages-cache-v1';
+
 self.addEventListener('install', function(event) {
+  var filesToCache = [
+
+    'style/styles.css',
+    'style/responsive.css',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/7.jpg',
+    'images/8.jpg',
+    'images/9.jpg',
+    'images/10.jpg',
+    'index.html',
+    'restaurant.html',
+    'js/main.js',
+    'js/resurant_info.js',
+    'js/dbhelper.js',
+    '/data/restaurants.json',
+    'https://maps.googleapi.com/js'
+  ];
   console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open('restaurant-cache-v1')
+    caches.open(staticCacheName)
     .then(function(cache) {
-		console.log(cache);
-      return cache.addAll(
-	   [
-    './style/styles.css',
-	'./style/responsive.css',
-    './images/1.jpg',
-	'./images/2.jpg',
-	'./images/3.jpg',
-	'./images/4.jpg',
-	'./images/5.jpg',
-	'./images/6.jpg',
-	'./images/7.jpg',
-	'./images/8.jpg',
-	'./images/9.jpg',
-	'./images/10.jpg',
-	'./index.html',
-	'./restaurant.html',
-	'./js/main.js',
-	'./js/resurant_info.js',
-	'./js/dbhelper.js',
-	'./data/restaurants.json',
- 
-]
-	  
-	  
-	  
-	  );
+      return cache.addAll(filesToCache);
     })
   );
 });
@@ -44,7 +44,7 @@ self.addEventListener('fetch', function(event) {
         return response;
       }
       console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-	})
-	);
+      return fetch(event.request);
+}));
+;
 });
