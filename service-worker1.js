ec7dc87a66211672f5815f9636fa7dc2b1a21167
Abukara <Lukas.Forst48@googlemail.com>
2018-06-10 21:03:18 +0200

    project review part 2
    project review part 2
    -sw update integration of indexed db
    -dbhelper changed from xhr to fetch to use server json response and not local json
    -added webapp-icons
    -added webp icons 
    -added manifest.json
    -added favicon

--- service-worker1.js
+++ service-worker1.js
@@ -1,84 +1,138 @@
-const cache_name = 'precache-v1';
-const urls_to_cache = ['css/styles.css',
-'css/responsive.css',
-'img/1.jpg',
-'img/2.jpg',
-'img/3.jpg',
-'img/4.jpg',
-'img/5.jpg',
-'img/6.jpg',
-'img/7.jpg',
-'img/8.jpg',
-'img/9.jpg',
-'img/10.jpg',
-'index.html',
-'restaurant.html',
-'js/main.js',
-'js/restaurant_info.js',
-'js/dbhelper.js',
-'data/restaurants.json',
-'https://maps.googleapi.com/js'
-];
+importScripts('/js/idb.js');
 
-self.addEventListener('install', (event) => {
-    console.info('Event: Install');
+//use of from https://github.com/GoogleChromeLabs/airhorn/blob/master/app/sw.js
 
-    event.waitUntil(
-      caches.open(cache_name)
-      .then((cache) => {
-        //[] of files to cache & if any of the file not present `addAll` will fail
-        return cache.addAll(urls_to_cache)
-        .then(() => {
-          console.info('All files are cached');
-          return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
-        })
-        .catch((error) =>  {
-          console.error('Failed to cache', error);
-        })
-      })
-    );
-  });
+let cache_name = 'precache-v1';
 
 
-  self.addEventListener('fetch', (event) => {
-  console.info('Event: Fetch');
+function startdb() {
+  idb.open('restaurant', 1, function (upgradeDB) {
+    //create Db objectStore for restaurants
+    upgradeDB.createObjectStore('restaurants', {keyPath: 'id'});
+  })
+}
 
-  var request = event.request;
-
-  //Tell the browser to wait for newtwork request and respond with below
-  event.respondWith(
-    //If request is already in cache, return it
-    caches.match(request).then((response) => {
-      if (response) {
-        return response;
-      }
-
-      //if request is not cached, add it to cache
-      return fetch(request).then((response) => {
-        var responseToCache = response.clone();
-        caches.open(cache_name).then((cache) => {
-            cache.put(request, responseToCache).catch((err) => {
-              console.warn(request.url + ': ' + err.message);
-            });
-          });
-
-        return response;
-      });
-    })
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    startdb()
+  )
 });
 
-/*
-  ACTIVATE EVENT: triggered once after registering, also used to clean up caches.
-*/
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-//Adding `activate` event listener
-self.addEventListener('activate', (event) => {
-  console.info('Event: Activate');
 
-  //Remove old and unwanted caches
+function addDB(values){
+
+  idb.open('restaurant',1).then(function (db){
+    var transaction = db.transaction('restaurants', 'readwrite');
+    var store = transaction.objectStore('restaurants');
+    return Promise.all(values.map(function (value){
+
+      return store.put(value);
+
+    })).then(function(e){
+
+    }).catch(function(e){
+      transaction.abort();
+      console.log(e);
+    })
+  })
+}
+
+//orientation at https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+addEventListener('fetch', event => {
+  event.respondWith(async function () {
+    //checks for call to restaurant
+    if (checkrestaurantid(event.request.url)) {
+
+
+      return idb.open('restaurant', 1).then(function(db){
+        var transaction = db.transaction('restaurants', 'readonly');
+        var store = transaction.objectStore('restaurants');
+
+        // Return items from database
+        return store.getAll();
+      }).then((dbdata) => {
+
+      //checks for length of indexdb if its empty load data into database
+        if (!dbdata.length) {
+
+          // returns  data from network
+          return fetch(event.request.url)
+            .then((responseUrl) => {
+
+              // return response;
+              return responseUrl.json()
+                .then(function (values) {
+
+                  // Adds data to database
+                  addDB(values);
+
+                  console.log("Saving to DB");
+                  var init = {
+                    status: 200,
+                    statusText: "OK",
+                    headers: {'Content-Type': 'application/json'}
+                  };
+
+                  const fetchResponse = new Response(JSON.stringify(values), init);
+                  return fetchResponse;
+                })
+              })
+            }else{
+              var init = {
+                status: 200,
+                statusText: "OK",
+                headers: {'Content-Type': 'application/json'}
+              };
+              // stringify Json to keep it in db.
+              const indexDBResponse = new Response(JSON.stringify(dbdata), init);
+              return indexDBResponse;
+            }
+          })
+
+          }else{
+      // Try to get the response from a cache.
+      var cachedResponse = await caches.match(event.request);
+
+      // Return it the response if one is foun.
+      if (cachedResponse) {
+
+        return cachedResponse;
+      }
+      // If there was no match in the cache the network will be used.
+
+      return fetch(event.request)
+        .then(function (cachedResponse) {
+
+          return caches.open(cache_name).then(function (cache) {
+            // try to not cache google maps
+            if (event.request.url.indexOf('maps') < 0) {
+              cache.put(event.request.url, cachedResponse.clone());
+            }
+            return cachedResponse;
+          });
+        });
+    }
+  }());
+});
+//checks restaurant id so restaurants can get cached and later be stored in idb.
+function checkrestaurantid(url){
+  var serverurl = /^http:\/\/localhost:1337\/restaurants$/;
+  var serverUrlMatch = url.match(serverurl);
+  if(serverUrlMatch)
+    return 1;
+  return 0;
+}
+
+self.addEventListener('activate', function (event) {
+  console.log('Activating new service worker...');
+//Remove old and unwanted caches
   event.waitUntil(
-    caches.keys().then((cache_name) => {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
         cache_name.map((cache) => {
           if (cache !== cache_name) {     //cacheName = 'cache-v1'
